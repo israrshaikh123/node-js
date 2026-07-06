34d3858296e50e431f803fb43c83c9b237089ece
israr shaikh <awmop968@gmail.com>
2026-07-06 19:04:29 +0530

    Add cookie-based authentication to AdminPanel: signup, login, middleware, logout

--- AdminPanel/controllers/dashboardController.js
+++ AdminPanel/controllers/dashboardController.js
@@ -1,19 +1,64 @@
+const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
 const dashboardController = {
-    index : (res , req  ) => {
-        req.render('dashboard')
-    },
-    tables : (res , req  ) => {
-        req.render('tables')
-    },
-    forms : (res , req  ) => {
-        req.render('forms')
-    },
-    login : (res , req  ) => {
-        req.render('login')
-    },
-    mailbox : (res , req  ) => {
-        req.render('mailbox')
-    },
+  index: (req, res) => {
+    res.render("dashboard", { activePage: "dashboard" });
+  },
+  tables: (req, res) => {
+    res.render("tables", { activePage: "tables" });
+  },
+  forms: (req, res) => {
+    res.render("forms", { activePage: "forms" });
+  },
+  login: (req, res) => {
+    res.render("login", { activePage: "login" });
+  },
+  mailbox: (req, res) => {
+    res.render("mailbox", { activePage: "mailbox" });
+  },
 
-}
-module.exports = dashboardController
+  signup: (req, res) => {
+    res.render("signup");
+  },
+
+  signupSubmit: async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    await User.create({
+      email: email,
+      name: name,
+      password: hashedPassword,
+    });
+    res.redirect("/");
+  },
+
+  loginSubmit: async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    console.log(user);
+    if (!user) {
+      res.send("User Not Found");
+    } else {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      console.log(isMatch);
+
+      if (isMatch === true) {
+        res.cookie("userId", user._id);
+        res.redirect("/");
+      } else {
+        res.send("Invalaid Password");
+      }
+    }
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("userId");
+    res.redirect("/login");
+  },
+};
+module.exports = dashboardController;
--- BookStore/models/bookModel.js
+++ BookStore/models/bookModel.js
@@ -4,7 +4,7 @@ const bookSchema = new mongoose.Schema({
     title : {type : String , required : true },
     author : {type : String , required : true},
     category : {type : String , required : true},
-    price : {type : Number , required : true},
+    price : {type  : Number , required : true},
     quantity : {type : Number , required : true},
     description : {type : String , required : true},
     image : {type : String , required : true},
